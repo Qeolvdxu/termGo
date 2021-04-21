52506f7e58c006720713baac8659e6db5b455b40
Hayden <57072675+Qeolvdxu@users.noreply.github.com>
2021-04-20 23:25:50 -0500

    Update and rename testBoard.cpp to termGo.cpp

--- termGo.cpp
+++ termGo.cpp
@@ -7,19 +7,6 @@ using namespace std;
 int main(void)
 {
 	Board* testBoard = new Board(18);
-	testBoard->placeStone(3,2);
-	testBoard->placeStone(3,3);
-	testBoard->placeStone(3,4);
-	testBoard->placeStone(3,5);
-	testBoard->placeStone(4,2);
-	testBoard->placeStone(5,2);
-	testBoard->placeStone(4,5);
-	testBoard->placeStone(5,5);
-	testBoard->placeStone(6,2);
-	testBoard->placeStone(6,3);
-	testBoard->placeStone(6,4);
-	testBoard->placeStone(6,5);
-
 	int row, column;
 	bool running = true;
 	bool safe = false;
